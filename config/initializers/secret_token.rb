d5068ddf9f7a286ccabff588e581b3ed2df3559e
yongfook <yongfook@gmail.com>
2012-09-03 08:23:40 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uberflirt::Application.config.secret_token = 'd708f993f69aefc6473010eb7e91cb28ded1ed2371024f2bd0e05639a056f1b4a935b7acb12a3d81fce2f9a913c9c35261beb4a844eebccdb2e1b2cd42d88394'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Uberflirt::Application.config.session_store :cookie_store, key: '_uberflirt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Uberflirt::Application.config.session_store :active_record_store
