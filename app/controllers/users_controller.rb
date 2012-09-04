510d9cd5676918b559cdfbe4d039a1378ffac7c9
yongfook <yongfook@gmail.com>
2012-09-04 08:08:29 +0800

    barebones styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
+    @likes_me = @user.likes_me    
+    @i_like = @user.i_like
+    #create array of instagram ids
+    @all = @likes_me.slice(0,19) + @i_like.slice(0,19)
+    @ids = @all.collect{|c| c["id"]}
+    @profiles = User.select("username, data, instagram_id").find_all_by_instagram_id(@ids)
   end
   
 end
